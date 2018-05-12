5d9f8ff4b9fb6f5d7efc2539f20dfdfbf3de9995
daikikojima <daikikojima0531@icloud.com>
2018-05-12 11:19:10 +0900

    MainActivity.kt: add library to MainActivity

--- app/src/main/java/com/jiko_daiki/mictest/MainActivity.kt
+++ app/src/main/java/com/jiko_daiki/mictest/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.jiko_daiki.mictest
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.*
 
 class MainActivity : AppCompatActivity() {
 
