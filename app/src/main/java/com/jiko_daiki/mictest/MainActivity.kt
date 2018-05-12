1407dc88349343ce3d9e72a41aeb91d2167ed87a
daikikojima <daikikojima0531@icloud.com>
2018-05-12 11:30:28 +0900

    MainActivity.kt: add Snackbar function and enums

--- app/src/main/java/com/jiko_daiki/mictest/MainActivity.kt
+++ app/src/main/java/com/jiko_daiki/mictest/MainActivity.kt
@@ -19,4 +19,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun createSnack(type: SnackType){
+        val snackbar:Snackbar
+        snackbar = when(type){
+            SnackType.ENABLE ->{
+                Snackbar.make(rootLayout, "Mic enabled", Snackbar.LENGTH_SHORT)
+            }
+            SnackType.DENIED -> {
+                Snackbar.make(rootLayout, "Permission Denied", Snackbar.LENGTH_LONG)
+            }
+            SnackType.NEVER -> {
+                Snackbar.make(rootLayout, "go setting and give permission", Snackbar.LENGTH_LONG)
+            }
+        }
+        when(type){
+            SnackType.ENABLE, SnackType.DENIED -> {
+                snackbar.setAction("Close", View.OnClickListener {
+                    snackbar.dismiss()
+                }).show()
+            }
+            SnackType.NEVER->{
+                snackbar.setAction("Go To App Setting", View.OnClickListener {
+                    val intent = Intent()
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.setData(Uri.fromParts("package", packageName, null))
+                    startActivity(intent)
+                })
+                .show()
+            }
+        }
+    }
+
+    enum class SnackType{ENABLE, DENIED, NEVER}
 }
